0842db7f359350cdaf77b99dbaa6a0fea8fdac82
Karo7787 <karooko77@proton.me>
2025-09-04 23:05:37 +0200

    Create fxmanifest.lua

---
+++ myfivemserv/ate_pingpong_paddle/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+autor 'Atenea'
+description 'Weapon Ping Pong Paddle'
+
+files{
+	'**/weaponarchetypes.meta',
+	'**/weaponanimations.meta',
+	'**/pedpersonality.meta',
+	'**/weapons.meta',
+}
+
+data_file 'WEAPON_METADATA_FILE' '**/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' '**/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' '**/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE' '**/weapons.meta'
+
+client_script 'cl_weaponNames.lua'
+
+dependency '/assetpacks'
